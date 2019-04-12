eabc4c6ad8eebf8fbb1200e71bd7a7dfcbc65563
Tommy Chong <tommy.chong@ha.org.hk>
2019-04-12 14:32:37 +0800

    -

--- src/components/beer/__tests__/InputArea-test.tsx
+++ src/components/beer/__tests__/InputArea-test.tsx
@@ -14,22 +14,20 @@ describe("InputArea", () => {
   it("should accept input", () => {
     const onSubmitSpy = jest.fn();
     const wrapper = mount(<InputArea onSubmit={onSubmitSpy} />);
-    const input = wrapper.find("input");
-    input.simulate("change", { target: { name: "text", value: "Resin" } });
+    wrapper
+      .find("input")
+      .simulate("change", { target: { name: "text", value: "Resin" } });
     expect(wrapper.find("input").props().value).toEqual("Resin");
   });
 
   it("should call onSubmit", done => {
     const onSubmitSpy = jest.fn();
     const wrapper = mount(<InputArea onSubmit={onSubmitSpy} />);
-
     wrapper.find("input").simulate("change", {
       target: { name: "text", value: "Octoberfest" }
     });
-
     wrapper.find("form").simulate("submit");
     expect(wrapper.find('button[type="submit"]').props().disabled).toBeTruthy();
-
     setImmediate(() => {
       expect(onSubmitSpy).toBeCalled();
       expect(onSubmitSpy).toBeCalledWith("Octoberfest");
@@ -40,9 +38,7 @@ describe("InputArea", () => {
   it("should not call onSubmit if text is empty", done => {
     const onSubmitSpy = jest.fn();
     const wrapper = mount(<InputArea onSubmit={onSubmitSpy} />);
-
     wrapper.find("form").simulate("submit");
-
     setImmediate(() => {
       expect(onSubmitSpy).not.toBeCalled();
       done();
